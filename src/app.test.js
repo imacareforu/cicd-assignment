95b5555f3bdcc589d9a8036f12fe63e3486a4965
imacareforu <imacareforu@gmail.com>
2025-04-07 11:38:45 +0700

    Fix build with legacy OpenSSL

--- src/app.test.js
+++ src/app.test.js
@@ -1,9 +1,9 @@
 const { dayOfTheWeek, monthOfYear } = require('./app');
 
-test('returns correct day', () => {
+test('dayOfTheWeek returns correct day', () => {
   expect(dayOfTheWeek(new Date('3/11/2020'))).toBe('Wednesday');
 });
 
-test('returns correct month', () => {
+test('monthOfYear returns correct month', () => {
   expect(monthOfYear(new Date('3/11/2020'))).toBe('March');
 });
